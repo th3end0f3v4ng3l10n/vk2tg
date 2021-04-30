988a4b9f5638dcf18581799edce479ca3135717e
twelve <th3end0f3v4ng3l10n@gmail.com>
2021-04-30 22:59:02 +0500

    get ts, server, key

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from colorama import Fore, Style
 import os
 import sys
 import configparser
-
-
-
+import requests
+import json
 def fprint(text,color):
     """Log Function"""
     if color == "red":
@@ -43,24 +42,45 @@ class Other:
 
         tg_token = config['Telegram']['Tg_Token']
         fprint("Read data from config.ini...", "yellow")
-        
 
 
+
+class WorkWithVkApi:
+    
+    def get_parametrs(self):
+        """Get Server, Key, Ts"""
+        global key, ts, server
+        response = requests.get('https://api.vk.com/method/messages.getLongPollServer?access_token=' + vk_token + '&v=5.21')
+        data = response.json()
+        #print(data)
+        server = data['response']['server']
+        server = server.replace("api.vk.com/ru", "")
+        key = data['response']['key']
+        ts = data['response']['ts']
+        print("server:",server);print("key:",key);print("ts:",ts) 
+
+    def get_messages(self):
+        """Get Messages From vk"""
+        response = requests.get('https://im.vk.com/n'+str(server)+'?act=a_check&key='+str(key)+'&wait=25&mode=2&ts='+str(ts)+'&version=3')
+        data = response.json()
+        print(data)
+
+    def 
 
 class Send_to_Telegram:
     def __init__(self):
         pass
 
-
-class Get_message:
-    def __init__(self):
-        pass
-
 def test():
+    print("--------------------------------")
     print(login, password, vk_token, tg_token)
+    print("--------------------------------")
 
 
 if __name__ == "__main__":
-    root = Other()
-    root.parse_config()
-    test()
+    root = Other() 
+    root.parse_config() #Parsing COnfig File
+    test() #Return to konsole data from config file
+    bb = WorkWithVkApi()
+    bb.get_parametrs()
+    bb.get_messages()
